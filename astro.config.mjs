59985b3fcda8bd9cd6e848e8a3e9a2bd9f68ce6b
Anurag Banerjee <anurag@axioned.com>
2024-12-29 17:52:17 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,22 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import cloudflare from "@astrojs/cloudflare";
-
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://anuragbanerjee.com",
-  output: "server",
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
     },
   }),
-
+  vite: {
+    optimizeDeps: {
+      exclude: ["react-compiler-runtime"],
+    },
+  },
   integrations: [tailwind({ applyBaseStyles: false }), sitemap(), react()],
 });
