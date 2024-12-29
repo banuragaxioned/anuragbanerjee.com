f2c59beec31b0d84fa682df673dbf37b330f7d62
Anurag Banerjee <anurag@axioned.com>
2024-12-29 17:10:08 +0530

    Configure sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://anuragbanerjee.com",
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
