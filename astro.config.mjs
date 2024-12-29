486256734a1aefee2acd108caa33c37afd313928
Anurag Banerjee <anurag@axioned.com>
2024-12-29 17:10:46 +0530

    Add React

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import tailwind from "@astrojs/tailwind";
 
 import sitemap from "@astrojs/sitemap";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://anuragbanerjee.com",
@@ -16,5 +18,5 @@ export default defineConfig({
     },
   }),
 
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), react()],
 });
