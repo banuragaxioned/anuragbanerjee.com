63f509ad50aba24784c1f357438614f2209f0dd4
Anurag Banerjee <anurag@axioned.com>
2025-03-15 20:37:00 +0530

    chore: package updates (including tailwind 4)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,23 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://anuragbanerjee.com",
+
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
     },
   }),
-  integrations: [tailwind({ applyBaseStyles: false }), sitemap()],
+
+  integrations: [sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
