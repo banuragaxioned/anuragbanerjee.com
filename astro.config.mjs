13f3ae0f5af6ac7b7ab03f96d989ebcbacc8ae24
Anurag Banerjee <anurag@axioned.com>
2025-05-20 23:55:20 +0530

    fix: resolve react 19 cf worker issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,29 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 import sitemap from "@astrojs/sitemap";
-
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://anuragbanerjee.com",
-
+  output: "server",
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
     },
   }),
-
   integrations: [sitemap(), react()],
-
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+      alias: import.meta.env.PROD
+        ? {
+            "react-dom/server": "react-dom/server.edge",
+          }
+        : undefined,
+    },
   },
 });
